0a78e4d1a6c87c136d1618cd05579a0be430417e
samopocuc <samo.pocuc@gmail.com>
2026-01-08 15:10:46 +0100

    add wrapper

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Menu from "./Components/Menu/Menu";
 import Logo from "./Components/Logo/Logo";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Wrapper from "./Components/Card/Wrapper/Wrapper";
+import Card from "./Components/Card/Card";
+
 export function App() {
 
     const logo = new URL('./assets/logo.svg', import.meta.url);
     const love = new URL('./assets/love.svg', import.meta.url);
+    const add = new URL('./assets/add.svg', import.meta.url);
 
     return (
         <>
@@ -17,6 +21,13 @@ export function App() {
                     { name: "About", link: "#about"}
                 ]}/>
             </Header>
+            <Wrapper>
+                <Card></Card>
+                <Card>
+                    <img src={add} alt="add"/>
+                    <p>Add New Location</p>
+                </Card>
+            </Wrapper>
             <Footer>
                 <span>Made with by <img src={love} alt="love"/>Victor Alvarado</span>
             </Footer>
