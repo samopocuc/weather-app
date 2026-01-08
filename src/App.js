fd4e6dc9582616b5f418ad782e930a711ac4912d
samopocuc <samo.pocuc@gmail.com>
2026-01-08 14:39:03 +0100

    add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Menu from "./Components/Menu/Menu";
-
+import Logo from "./Components/Logo/Logo";
+import Header from "./Components/Header/Header";
 export function App() {
+
+    const logo = new URL('./assets/logo.svg', import.meta.url);
+
     return (
         <>
-            <h1>Parcel React App</h1>
-            <Menu items={[
-                { name: "Github", link: "/" },
-                { name: "About", link: "#about"}
-            ]}/>
-            <p>Edit <code>src/App.tsx</code> to get started!</p>
+            <Header>
+                <Logo title="Weather by VicBox" url={logo} />
+                <Menu items={[
+                    { name: "Github", link: "/" },
+                    { name: "About", link: "#about"}
+                ]}/>
+            </Header>
         </>
     );
 }
